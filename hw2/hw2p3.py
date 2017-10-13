c13272042ca2fadccd1cf0b0823a573d2c98b2b8
kevche <kevche@umich.edu>
2017-10-12 22:57:04 -0400

    done hw2

---
+++ hw2/hw2p3.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+## 3a: Read training data
+word_idx = np.array(range(1,1449))
+train_dicts = [{ x:1 for x in word_idx},{ x:1 for x in word_idx} ] # init both cnt with 1 -> Laplace smoothing
+trainReader = csv.reader(open('data/spam_classification/SPARSE.TRAIN', newline=''), delimiter=' ', quotechar='|')
+for row in trainReader:
+    if int(row[0])==1: 
+        for wc in row[2:]:
+            key,value = wc.split(":")
+            key,value = int(key),int(value)
+            train_dicts[0][key] = train_dicts[0][key] + value
+    elif int(row[0])==-1:
+        for wc in row[2:]:
+            key,value = wc.split(":")
+            key,value = int(key),int(value)
+            train_dicts[1][key] = train_dicts[1][key] + value
+sum_spam = sum(train_dicts[0].values())
+sum_nospam = sum(train_dicts[1].values())
+
+
+# Test, Evaluation
+error_cnt = 0
+testReader = csv.reader(open('data/spam_classification/SPARSE.TEST', newline=''), delimiter=' ', quotechar='|')
+for row in testReader:
+    w_given_spam = 0
+    w_given_nospam = 0
+    gt = int(row[0])
+    for wc in row[2:]:
+        key,value = wc.split(":")
+        key,value = int(key),int(value)
+        w_given_spam = w_given_spam + value*np.log(train_dicts[0][key]/sum_spam)
+        w_given_nospam = w_given_nospam + value*np.log(train_dicts[1][key]/sum_nospam)
+
+    if (w_given_spam>w_given_nospam and gt==-1) or (w_given_spam<w_given_nospam and gt==1):
+        error_cnt = error_cnt + 1
+
+print ("---hw2-p3a---")
+print ("Error Rate: %s %%" % (error_cnt / testReader.line_num * 100))
+
+## Q3b: Finding 5-most indicative tokens
+score_list = np.zeros(len(word_idx))
+for i,w_idx in enumerate(word_idx):
+    score_list[i] = np.log( (train_dicts[0][w_idx]/sum_spam) / (train_dicts[1][w_idx]/sum_nospam) )
+    s_idx = score_list.argsort()[-5:]
+
+print ("---hw2-p3b---")
+for line in open('data/spam_classification/TOKENS_LIST'):
+    if (int(line.split()[0])-1) in s_idx:
+        top_5_result = line.split()[1]
+        print (top_5_result)
+
+plt.plot(word_idx, score_list, 'o', markersize=2)
+plt.plot(word_idx[s_idx], score_list[s_idx], 'ro', markersize=5)
+
+plt.axis([0, 1449, -10, 10])
+plt.xlabel("Word Index")
+plt.ylabel("Indicative Score")
+plt.show()
--- hw2/hw2p4.py
+++ hw2/hw2p4.py
@@ -3,15 +3,19 @@
 import numpy as np
 from numpy import linalg as LA
 import scipy.io as sio
-
 from multiprocessing import Pool
+import matplotlib.pyplot as plt
 
+# Q4
 mnist_data = sio.loadmat('Data/mnist_data.mat')
-num_sample = 100
+num_sample = 1000
 
+def l1(diff):
+    return LA.norm(diff,axis=1)
+def l2(diff):
+    return np.sum(np.abs(diff),axis=1)
 
-def mnist_knn(num_knn):
-    # num_knn = 5 
+def mnist_knn(num_knn,dist_f):
     # Test data for eval
     sampled_idx = np.random.choice(mnist_data['test'].shape[0],num_sample)
     sampled_test_data = mnist_data['test'][sampled_idx,:]
@@ -24,9 +28,7 @@ def mnist_knn(num_knn):
     acc = 0
     for i in range(num_sample):
         # Infer: find knn
-        
-        # dist_lst = LA.norm(mnist_data['train'][:,1:] - sampled_test_data[i,1:],axis=1) # Use L2 norm
-        dist_lst = np.amax(mnist_data['train'][:,1:] - sampled_test_data[i,1:],axis=1)
+        dist_lst = dist_f(mnist_data['train'][:,1:] - sampled_test_data[i,1:])
         k_idx = dist_lst.argsort()[:num_knn]
 
         # Eval: compare y_bar & y
@@ -36,11 +38,29 @@ def mnist_knn(num_knn):
 
         if yi_bar == int(gt_test[i]):
             acc += 1
-    print("Take k=%s, ACC=%s" % (num_knn,acc/num_sample))
 
+    print("Take k=%s, Test Accuracy=%s" % (num_knn, acc/num_sample))
+    return acc/num_sample
 
 if __name__ == '__main__':
 
     knn_arr = [1,5,9,13] # k=1,5,9,13
+    l1_arr,l2_arr = [l1]*4,[l2]*4
     p = Pool(4)
-    p.map(mnist_knn,knn_arr)
+    print("---hw2-p4a---") 
+    l1_r = p.starmap(mnist_knn,zip(knn_arr,l1_arr))
+    l2_r = p.starmap(mnist_knn,zip(knn_arr,l2_arr))
+    print(l1_r)
+    print(l2_r)
+
+    plt.plot(knn_arr, l1_r,marker='x',linestyle='-',color='b',label="L1")
+    plt.plot(knn_arr, l2_r,marker='o',linestyle='-',color='r',label="L2")
+    plt.ylim([0.5,1])
+    plt.xlabel("kNN")
+    plt.ylabel("Test Accuracy")
+    plt.legend()
+    plt.show()
+
+
+
+
